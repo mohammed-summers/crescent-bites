09c6b9e749ad6a8b4efb368bc56cdf896212f263
mohammed <mohammed.k.summers@gmail.com>
2024-04-22 17:12:32 -0500

    First commit

---
+++ src/main/java/com/mks/crescentbites/CrescentBitesApplication.java
@@ -0,0 +1,13 @@
+package com.mks.crescentbites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrescentBitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrescentBitesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mks/crescentbites/CrescentBitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mks.crescentbites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrescentBitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
